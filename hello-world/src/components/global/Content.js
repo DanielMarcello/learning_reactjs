30d392cd9a491ba9f9489b71fc6dd0853203a596
DanielMarcello <daniel.salinas.tellez@gmail.com>
2018-08-14 09:32:29 -0700

    app structure and components distribution

---
+++ hello-world/src/components/global/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <h1>I am the content</h1>
+        <p> bla bla bla...</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
