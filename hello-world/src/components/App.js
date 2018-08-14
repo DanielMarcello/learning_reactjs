d2e553362de171d19e2c3e2a5c7b22d6619a7855
DanielMarcello <daniel.salinas.tellez@gmail.com>
2018-08-14 10:30:03 -0700

    proptypes

--- hello-world/src/components/App.js
+++ hello-world/src/components/App.js
@@ -1,17 +1,21 @@
+//Dependencies
 import React, { Component } from 'react';
 
 //Components
-import Header from './global/Header'
-import Content from './global/Content'
-import Footer from './global/Footer'
+import Header from './global/Header';
+import Content from './global/Content';
+import Footer from './global/Footer';
+
+//Data
+import items from '../data/menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header title="Codejobs" items={items}/>
         <Content/>
-        <Footer/>
+        <Footer copyright="&copy; Codejobs 2018"/>
       </div>
     );
   }
